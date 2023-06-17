a72894db4d39be84a603d97fb18630bfe0646d64
Фирдавс Ибрагимов <firdavs.ibra04@gmail.com>
2023-06-17 17:07:50 +0500

    chat added

--- src/app/main/components/chat/chat.component.ts
+++ src/app/main/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMessage } from 'src/app/models/message';
 
 @Component({
   selector: 'app-delete-question',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent {
+  inputMessage = '';
+  messages: IMessage[] = [];
+
+  sendMessage() {
+    if(!this.inputMessage || !this.inputMessage.trim()){
+      return;
+    }
+
+    const userMessage: IMessage = {
+      id: this.messages.length + 1,
+      senderId: 1,
+      receiverId: 2,
+      message: this.inputMessage.trim(),
+      timestamp: new Date().toISOString() 
+    }
+
+    this.messages.push(userMessage);
+    this.inputMessage = '';
+  }
 
 }
